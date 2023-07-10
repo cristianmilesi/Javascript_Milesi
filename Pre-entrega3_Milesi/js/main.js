bf5a3d995a96347a4006bf30e7f35d536b952828
cristianmilesi <cristianmilesi.dev@gmail.com>
2023-07-10 16:51:55 -0300

    Pre-entrega 3: Primera parte

---
+++ Pre-entrega3_Milesi/js/main.js
@@ -0,0 +1,8 @@
+let comprarMedalla = document.getElementById("medallasBtn")
+let comprarBandana = document.getElementById("bandanasBtn")
+let comprarCollar = document.getElementById("collaresBtn")
+
+comprarMedalla.addEventListener("click", ()=>{
+    console.log("Botón de Comprar Medalla funciona")
+})
+
